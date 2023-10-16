13273339e586055a962ad5107b1a89da5bb2f724
Monali Valunj <monavalunj96@gmail.com>
2023-10-15 22:25:22 -0700

    intial comiit

---
+++ js/01-functions-practice.js
@@ -0,0 +1,27 @@
+//STEP 1
+// function halfNumber(num){
+//    return console.log(`Half of ${num} is ${num/2}`)
+// }
+// halfNumber(5) 
+
+
+//STEP 2
+// function squareNumber(n) {
+//    return console.log(`The result of squaring the number ${n}  is ${n*n}.`)
+// }
+// squareNumber(3)
+
+
+//STEP 3
+// function percentOf(num1, num2) {
+//     return console.log(`${num1} is ${(100 * num1) / num2}% of ${num2}.`)
+// }
+// percentOf(2,4)
+
+
+//STEP 4
+// function findModulus(n1, n2){
+//     return console.log(`${ n1 % n2 } is the modulus of ${n1} and ${n2}.`)
+// }
+// findModulus(4, 10)
+
